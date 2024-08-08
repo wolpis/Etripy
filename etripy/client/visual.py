18a3a3400faeb0187a8fde6c037a47a4ed7303f4
VoidAsMad <voidasmad@gmail.com>
2024-08-09 00:41:38 +0900

    feat(visual) : 이미지 기능 완성

--- etripy/client/visual.py
+++ etripy/client/visual.py
@@ -3,7 +3,12 @@ from typing import Optional
 
 from etripy.error import VisualImageException
 from etripy.http import EtriRequest
-from etripy.model.visual.image import ObjectDetectResult
+from etripy.model.visual.image import (
+    FaceDeIDResult,
+    HumanParsingResult,
+    HumanStatusResult,
+    ObjectDetectResult,
+)
 
 
 class ImageClient(EtriRequest):
@@ -31,3 +36,66 @@ class ImageClient(EtriRequest):
         return [
             ObjectDetectResult(data=data_) for data_ in result["return_object"]["data"]
         ]
+
+    async def human_parsing(
+        self, file_path: str, file_type: str = "auto"
+    ) -> Optional[list[HumanParsingResult]]:
+        if file_type == "auto":
+            file_type = file_path.split(".")[-1]
+
+        file = open(file_path, "rb")
+        imageContents = base64.b64encode(file.read()).decode("utf8")
+        file.close()
+
+        data = {"argument": {"type": file_type, "file": imageContents}}
+        result = await self.request(method="POST", endpoint="/HumanParsing", data=data)
+        try:
+            if result["return_object"] == {}:
+                return None
+        except KeyError:
+            if result["result"] != "0":
+                raise VisualImageException(result["reason"])
+
+        keys = list(result["return_object"].keys())
+        return [HumanParsingResult(data=result["return_object"][key]) for key in keys]
+
+    async def face_deid(self, file_path: str):
+        file = open(file_path, "rb")
+        imageContents = base64.b64encode(file.read()).decode("utf8")
+        file.close()
+
+        data = {"argument": {"file": imageContents, "type": "1"}}
+        result = await self.request(method="POST", endpoint="/FaceDeID", data=data)
+        try:
+            if result["return_object"] == {}:
+                return None
+        except KeyError:
+            if result["result"] != "0":
+                raise VisualImageException(result["reason"])
+        return [
+            FaceDeIDResult(data=data_) for data_ in result["return_object"]["results"]
+        ]
+
+    async def human_status(
+        self, file_path: str, file_type: str = "auto"
+    ) -> Optional[list[HumanStatusResult]]:  ## 현재 API 이슈 있음
+        if file_type == "auto":
+            file_type = file_path.split(".")[-1]
+
+        file = open(file_path, "rb")
+        imageContents = base64.b64encode(file.read()).decode("utf8")
+        file.close()
+
+        data = {"argument": {"type": file_type, "file": imageContents}}
+        result = await self.request(method="POST", endpoint="/HumanStatus", data=data)
+        try:
+            if result["return_object"] == {}:
+                return None
+        except KeyError:
+            if result["result"] != "0":
+                raise VisualImageException(result["reason"])
+        return [
+            HumanStatusResult(data=data_)
+            for data_ in result["return_object"]["results"]
+        ]
+        print(result)
--- etripy/sync/visual.py
+++ etripy/sync/visual.py
@@ -2,7 +2,12 @@ import base64
 from typing import Optional
 
 from etripy.error import VisualImageException
-from etripy.model.visual.image import ObjectDetectResult
+from etripy.model.visual.image import (
+    FaceDeIDResult,
+    HumanParsingResult,
+    HumanStatusResult,
+    ObjectDetectResult,
+)
 from etripy.sync.http import SyncEtriRequest
 
 
@@ -30,4 +35,68 @@ class ImageClient(SyncEtriRequest):
                 raise VisualImageException(result["reason"])
         return [
             ObjectDetectResult(data=data_) for data_ in result["return_object"]["data"]
+        ]
+
+    def human_parsing(
+        self, file_path: str, file_type: str = "auto"
+    ) -> Optional[list[HumanParsingResult]]:
+        if file_type == "auto":
+            file_type = file_path.split(".")[-1]
+
+        file = open(file_path, "rb")
+        imageContents = base64.b64encode(file.read()).decode("utf8")
+        file.close()
+
+        data = {"argument": {"type": file_type, "file": imageContents}}
+        result: dict[str, dict[str, str]] = self.request(
+            method="POST", endpoint="/HumanParsing", data=data
+        )
+        try:
+            if result["return_object"] == {}:
+                return None
+        except KeyError:
+            if result["result"] != "0":
+                raise VisualImageException(result["reason"])
+
+        keys = list(result["return_object"].keys())
+        return [ObjectDetectResult(data=result["return_object"][key]) for key in keys]
+
+    def face_deid(self, file_path: str):
+        file = open(file_path, "rb")
+        imageContents = base64.b64encode(file.read()).decode("utf8")
+        file.close()
+
+        data = {"argument": {"file": imageContents, "type": "1"}}
+        result = self.request(method="POST", endpoint="/FaceDeID", data=data)
+        try:
+            if result["return_object"] == {}:
+                return None
+        except KeyError:
+            if result["result"] != "0":
+                raise VisualImageException(result["reason"])
+        return [
+            FaceDeIDResult(data=data_) for data_ in result["return_object"]["results"]
+        ]
+
+    def human_status(
+        self, file_path: str, file_type: str = "auto"
+    ) -> Optional[list[HumanStatusResult]]:
+        if file_type == "auto":
+            file_type = file_path.split(".")[-1]
+
+        file = open(file_path, "rb")
+        imageContents = base64.b64encode(file.read()).decode("utf8")
+        file.close()
+
+        data = {"argument": {"type": file_type, "file": imageContents}}
+        result = self.request(method="POST", endpoint="/HumanStatus", data=data)
+        try:
+            if result["return_object"] == {}:
+                return None
+        except KeyError:
+            if result["result"] != "0":
+                raise VisualImageException(result["reason"])
+        return [
+            HumanStatusResult(data=data_)
+            for data_ in result["return_object"]["results"]
         ]
